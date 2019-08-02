e18ff2e685f3c6c5a26b42d5fc6b26eec99f8aac
Mendez324 <48069230+Mendez324@users.noreply.github.com>
2019-08-02 17:12:37 +0100

    slider plugin implement and about.html eddited

--- Project/functions.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector('.c-mmenu__navigation');
-document.querySelector('.c-mmenu__mobile-burguer').addEventListener('click', function(e) {
-	e.preventDefault();
-	
-	this.classList.toggle('is-open'); // toggle vai adicionar/remover a cada click
-	nav.classList.toggle('is-open');
-});
-
-
